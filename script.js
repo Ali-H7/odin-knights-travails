6ad7e3f69c209fabff6ae6e26366d8e378d250b2
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-08-18 05:59:32 +0300

    Create methods for finding shortest path and complete the project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class Game {
-  constructor(boardSize) {
-    this.graph = this.generateGraph(boardSize);
+  constructor() {
+    this.graph = this.generateGraph(8);
   }
 
   generateGraph(size) {
@@ -49,7 +49,49 @@ class Game {
     }
     return allowedMoves.slice(8, 16);
   }
+
+  findShortestPath(start) {
+    const queue = [this.graph[start]];
+    queue[0].visited = true;
+    queue[0].path = queue[0].coords;
+
+    while (queue.length != 0) {
+      const currentTile = queue.shift();
+      for (let i = 0; i < currentTile.allowedMoves.length; i++) {
+        let linkedTile = currentTile.allowedMoves[i];
+        linkedTile = this.graph[linkedTile];
+        if (linkedTile.visited) continue;
+        linkedTile.visited = true;
+        linkedTile.path = currentTile;
+        queue.push(linkedTile);
+      }
+    }
+  }
+
+  printShortestPath(start, end) {
+    let endTile = this.graph[end];
+    let coords = endTile.coords;
+    const path = [];
+    while (!(coords[0] === start[0] && coords[1] === start[1])) {
+      path.push(endTile.coords);
+      endTile = endTile.path;
+      coords = endTile.coords;
+    }
+    path.push(endTile.coords);
+    path.reverse();
+    console.log(`You made it in ${path.length - 1} moves ! Here's your path:`);
+    path.forEach((tile) => console.log(tile));
+  }
+
+  knightMoves(start, end) {
+    this.findShortestPath(start);
+    this.printShortestPath(start, end);
+    //reset the graph
+    this.graph = this.generateGraph(8);
+  }
 }
 
-const game = new Game(8);
-console.log(game.graph);
+const game = new Game();
+game.knightMoves([3, 3], [3, 4]);
+game.knightMoves([3, 3], [7, 7]);
+game.knightMoves([0, 0], [7, 7]);
